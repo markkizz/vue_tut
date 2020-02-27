6eea2f0db111089eddf80af1b3f0f57efc04ac6c
markkizz <mark.pattanapara@gmail.com>
2020-02-27 12:59:48 +0700

    Add Doc to vue slot

--- intro_vue_basic/src/components/V-slotAndSlotProps/ClockParent.vue
+++ intro_vue_basic/src/components/V-slotAndSlotProps/ClockParent.vue
@@ -1,4 +1,7 @@
 <template>
+  <!-- 
+    v-slot for recieve data from Child component slot props will send All with object you should destructering it
+  -->
   <ClockChild v-slot="{time}">Parent clock: {{time}}</ClockChild>
 </template>
 
